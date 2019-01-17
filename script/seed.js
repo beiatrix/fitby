ebe8546843f8e72d5311bdf326ffd2b0d7ce19e2
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-17 15:12:23 -0500

    seed file update

--- script/seed.js
+++ script/seed.js
@@ -8,8 +8,18 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({
+      firstName: 'Cody',
+      lastName: 'Pug',
+      email: 'cody@email.com',
+      password: '123'
+    }),
+    User.create({
+      firstName: 'Murphy',
+      lastName: 'Mullaly',
+      email: 'murphy@email.com',
+      password: '123'
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
