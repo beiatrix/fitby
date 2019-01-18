6aa6082b1d44e53eb4fc427ad1b07cfb661ac1ea
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-18 11:08:19 -0500

    add nutrition

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Home} from './components'
+import {Login, Signup, UserHome, Home, Nutrition} from './components'
 import {me} from './store'
 
 /**
@@ -24,6 +24,7 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
+            <Route path="/nutrition" component={Nutrition} />
             <Route path="/home" component={UserHome} />
           </Switch>
         )}
