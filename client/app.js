05725b14d39ad4f5be4a38898fe484ea1c3d162e
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-18 13:05:55 -0500

    IMPLEMENTED PERSISTENT DRAWER

--- client/app.js
+++ client/app.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-import {Navbar, Footer} from './components'
+import {Navbar, Footer, PersistentDrawer} from './components'
 import Routes from './routes'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <PersistentDrawer />
+      {/* <Navbar /> */}
       <Routes />
       <Footer />
     </div>
