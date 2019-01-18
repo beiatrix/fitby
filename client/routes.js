0e83b7f0c8424f0f52f17a408906e9a306c3863d
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-18 11:27:33 -0500

    add fitness

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Home, Nutrition} from './components'
+import {Login, Signup, UserHome, Home, Nutrition, Fitness} from './components'
 import {me} from './store'
 
 /**
@@ -25,7 +25,8 @@ class Routes extends Component {
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/nutrition" component={Nutrition} />
-            <Route path="/home" component={UserHome} />
+            <Route path="/fitness" component={Fitness} />
+            <Route path="/home" component={Fitness} />
           </Switch>
         )}
         {/* Displays our Home component as a fallback */}
