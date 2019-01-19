1d998a4dbbffaf92a8eb07641fc7412e2bb585f4
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-19 16:23:53 -0500

    expansion panel!!!

--- client/components/addFood.js
+++ client/components/addFood.js
@@ -10,6 +10,15 @@ import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Button from '@material-ui/core/Button'
 import Checkbox from '@material-ui/core/Checkbox'
 
+import IconButton from '@material-ui/core/IconButton'
+import AddIcon from '@material-ui/icons/Add'
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
+import Typography from '@material-ui/core/Typography'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
 class AddFood extends Component {
   constructor() {
     super()
@@ -51,36 +60,41 @@ class AddFood extends Component {
 
     return (
       <div>
-        <form className={classes.form} onSubmit={this.handleSubmit}>
-          <CardContent>
-            {/* food input */}
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="food">add food</InputLabel>
-              <Input
-                onChange={this.handleChange}
-                name="food"
-                id="food"
-                value={this.state.name}
-              />
-            </FormControl>
-            {/* healthy? */}
-            <FormControlLabel
-              control={
-                <Checkbox
-                  onChange={this.handleCheck}
-                  value="true"
-                  color="default"
+        <div className={classes.root}>
+          <ExpansionPanel>
+            <ExpansionPanelSummary expandIcon={<AddIcon />} />
+            <form className={classes.form} onSubmit={this.handleSubmit}>
+              <CardContent>
+                {/* food input */}
+                <FormControl margin="none" required fullWidth>
+                  <InputLabel htmlFor="food">enter food</InputLabel>
+                  <Input
+                    onChange={this.handleChange}
+                    name="food"
+                    id="food"
+                    value={this.state.name}
+                  />
+                </FormControl>
+                {/* healthy? */}
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      onChange={this.handleCheck}
+                      value="true"
+                      color="default"
+                    />
+                  }
+                  label="healthy?"
                 />
-              }
-              label="healthy? *"
-            />
-          </CardContent>
-          <CardActions>
-            <Button type="submit" id="addFoodSubmit" fullWidth>
-              SUBMIT
-            </Button>
-          </CardActions>
-        </form>
+              </CardContent>
+              <CardActions>
+                <Button type="submit" id="addFoodSubmit" fullWidth>
+                  SUBMIT
+                </Button>
+              </CardActions>
+            </form>
+          </ExpansionPanel>
+        </div>
       </div>
     )
   }
